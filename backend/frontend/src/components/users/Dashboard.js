14b0b03b50504ab29c7a9773c6b481e0390c19e1
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2019-12-22 16:49:57 -0500

    Added additional features

---
+++ backend/frontend/src/components/users/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Form from './Form';
+import Users from './Users';
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <Form />
+            <Users />
+        </Fragment>
+    )
+}
