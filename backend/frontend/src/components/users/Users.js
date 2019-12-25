ea3c2e649c757c78766fafab390925672bf73c92
Nahzilian <50055124+Nahzilian@users.noreply.github.com>
2019-12-24 22:11:57 -0500

    added some more components

--- backend/frontend/src/components/users/Users.js
+++ backend/frontend/src/components/users/Users.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getUsers } from '../../actions/users';
 export class Users extends Component {
-    static PropTypes = {
+    static propTypes = {
         users: PropTypes.array.isRequired
     }
 
